f33038b4e6e01bee32b5186a30617a0f6477d795
gl0bix <46976350+gl0bix@users.noreply.github.com>
2022-05-22 17:27:34 +0200

    added clickable file list for player (currently only video) change record icon to indicator when clicked
    TODO: select different player for audio
    Signed-off-by: Daniel Schellenberg <schellen@th-brandenburg.de>

--- gui.py
+++ gui.py
@@ -1,5 +1,9 @@
+import os
+import threading
 import tkinter as tk
-import tkinter.filedialog as files
+from os import listdir
+from os.path import isfile, join
+from tkinter import filedialog
 
 import cv2
 import sounddevice
@@ -9,6 +13,8 @@ from PIL import Image, ImageTk
 import sound_recording
 import video_recording
 
+PATH = os.path.dirname(__file__)
+FILE_FORMATS = ('.avi', '.mp4', 'mp3', '.wav')
 
 # solution from: https://stackoverflow.com/questions/8044539/listing-available-devices-in-python-opencv
 def __get_video_devices():
@@ -25,11 +31,16 @@ def __get_video_devices():
 
 
 def init_main_window():
+
+    def __play_file(file):
+        print('playing file')
+        new_media = instance.media_new(file)
+        player.set_media(new_media)
+        t = threading.Thread(target=player.play)
+        t.start()
+
     window = tk.Tk()
     window.title('Tkinter Multimedia Player')
-
-    # window.tk.call("source", "assets/azure.tcl")
-    # window.tk.call("set_theme", "dark")
 
     # file actions
     # files.asksaveasfilename()
@@ -47,19 +58,21 @@ def init_main_window():
     icon_folder = Image.open('assets/images/icons/folder.png').resize((20, 20))
     icon_mic = Image.open('assets/images/icons/microphone.png').resize((20, 20))
     icon_cam = Image.open('assets/images/icons/video-camera.png').resize((20, 20))
+    icon_rec = Image.open('assets/images/icons/icon_record.png').resize((20, 20))
     icon_play = ImageTk.PhotoImage(icon_play)
     icon_pause = ImageTk.PhotoImage(icon_pause)
     icon_stop = ImageTk.PhotoImage(icon_stop)
     icon_folder = ImageTk.PhotoImage(icon_folder)
     icon_mic = ImageTk.PhotoImage(icon_mic)
     icon_cam = ImageTk.PhotoImage(icon_cam)
+    icon_rec = ImageTk.PhotoImage(icon_rec)
 
     # top menu  section
     frm_top = tk.Frame(master=window, width=500, height=20)
-    btn_folder = tk.Button(master=frm_top, image=icon_folder, width='20px', height='20px', command=files.askopenfile)
+    btn_folder = tk.Button(master=frm_top, image=icon_folder, width='20px', height='20px', command=filedialog.askopenfile)
     btn_folder.pack(side=tk.LEFT, padx=2)
 
-    #get input device options
+    # get input device options
     audio_input_devices = [(sounddevice.query_devices().index(device), device.get('name'),)
                      for device in sounddevice.query_devices()
                      if device.get('max_input_channels') > 0]
@@ -75,8 +88,6 @@ def init_main_window():
     lbl_audio_input_devices = tk.Label(master=frm_top, text="Select input device")
     drop_video_input_devices = tk.OptionMenu(frm_top, chosen_video_input_device, *video_input_devices)
     lbl_video_input_devices = tk.Label(master=frm_top, text="Select output device")
-    print(audio_input_devices)
-    print(video_input_devices)
 
     drop_audio_input_devices.pack(side=tk.RIGHT)
     lbl_audio_input_devices.pack(side=tk.RIGHT)
@@ -86,25 +97,35 @@ def init_main_window():
     # file list section
     frm_list = tk.Frame(master=window, width=300, height=500, bg="White", borderwidth=1)
 
+    files = [f for f in listdir(PATH) if isfile(join(PATH, f)) and f[-4:] in FILE_FORMATS]
+    print(*files)
+    lst_box_files = tk.Listbox(master=frm_list)
+    lst_box_files.pack()
+    for f in files:
+        lst_box_files.insert(tk.END, f)
+    frm_list.pack()
+
     # video player section
     frm_video = tk.Frame(master=window, width=500, height=500)
-
+    lbl_video = tk.Label(master=frm_video)
     instance = vlc.Instance()
     player = instance.media_player_new()
-    media = instance.media_new('assets/videos/example.mp4')
+    media = instance.media_new('')
     player.set_xwindow(frm_video.winfo_id())
     player.set_media(media)
     player.audio_set_volume(100)
 
     # bottom section
-    # TODO: indicators for recording, vidoe/audio state
     frm_bottom = tk.Frame(master=window, width=500, height=20)
 
     btn_audio_record = tk.Button(master=frm_bottom, image=icon_mic,
-                                 command=lambda: sound_recording.prepare_rec(int(chosen_audio_input_device.get()[1]), 'record_from_chosen_device'))
+                                 command=lambda: [sound_recording.prepare_rec(int(chosen_audio_input_device.get()[1]), 'record_from_chosen_device'),
+                                                  btn_audio_record.config(image=icon_rec, state='disabled')])
     btn_video_record = tk.Button(master=frm_bottom, image=icon_cam,
-                                 command=lambda: video_recording.prepare_rec(int(chosen_video_input_device.get()[1]), 'video_filename'))
-    btn_play = tk.Button(master=frm_bottom, image=icon_play, command=player.play)
+                                 command=lambda: [video_recording.prepare_rec(int(chosen_video_input_device.get()[1]), 'video_filename'),
+                                                  btn_video_record.config(image=icon_rec, state='disabled')])
+    btn_play = tk.Button(master=frm_bottom, image=icon_play,
+                         command=lambda: __play_file(lst_box_files.get(lst_box_files.curselection()[0])))
     btn_pause = tk.Button(master=frm_bottom, image=icon_pause, command=lambda: player.set_pause(1))
     btn_stop = tk.Button(master=frm_bottom, image=icon_stop, command='')
     btn_audio_record.pack(side=tk.LEFT, padx=2)
