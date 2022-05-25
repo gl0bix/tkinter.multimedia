52c96ce703151a2a3927c68d205c47c967d931d9
gl0bix <46976350+gl0bix@users.noreply.github.com>
2022-05-25 10:30:05 +0200

    Feature/windows (#3)
    * changed project structure,
    added icon for video recording,
    linked functions for recording to buttons (TODO: error from stop varibale),
    added selection for audio and video input
    
    Signed-off-by: Daniel Schellenberg <schellen@th-brandenburg.de>
    
    * added clickable file list for player (currently only video)
    change record icon to indicator when clicked
    
    TODO: select different player for audio
    Signed-off-by: Daniel Schellenberg <schellen@th-brandenburg.de>
    
    * - adjusted GUI
    - more functionalllity
    
    * - improved ExceptionHandling
    
    * - added os differenciation when setting media path
    - added bin icon for delete button,
    - added scrollbar for listbox and adjusted width
    - added channels from chosen device to recording parameter list to be used instead of hardcoded channel number (TODO: needs to be tested)
    
    Signed-off-by: Daniel Schellenberg <schellen@th-brandenburg.de>
    
    * - removed os differentiation when setting media path
    - bug fix: added missing "." in FILE_FORMATS at mp3
    
    Co-authored-by: cdwb1 <cdwb11@gmail.com>

--- sound_recording.py
+++ sound_recording.py
@@ -1,7 +1,6 @@
 import datetime
 import queue
 import threading
-import tkinter
 
 import numpy
 import sounddevice
@@ -9,10 +8,12 @@ import soundfile
 
 assert numpy
 
-global stop
+stop = False
 
 
-def start_recording(filename, device_id, sample_rate):
+def start_recording(filename, device_id, sample_rate, channels):
+    global stop
+    stop = False
     q = queue.Queue()
 
     # Function to Copy audio_block from InputStream into Queue
@@ -20,9 +21,9 @@ def start_recording(filename, device_id, sample_rate):
         q.put(audio_block.copy())
 
     # create new soundfile
-    with soundfile.SoundFile(filename, mode='w', samplerate=sample_rate, channels=2) as file:
+    with soundfile.SoundFile(filename, mode='w', samplerate=sample_rate, channels=channels) as file:
         # Start InputStream (Callback = called periodically with recorded Data)
-        with sounddevice.InputStream(samplerate=sample_rate, channels=2, device=device_id, callback=callback):
+        with sounddevice.InputStream(samplerate=sample_rate, channels=channels, device=device_id, callback=callback):
             # check if new element in Queue and write it in File
             while True:
                 file.write(q.get())
@@ -32,28 +33,16 @@ def start_recording(filename, device_id, sample_rate):
 
 def prepare_rec(device_id=1, filename=None):
     if not filename:
-        filename = "unnamed_" + datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S') + ".wav"
+        filename = "./media/rec_" + datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S') + ".wav"
     if filename.strip()[-3:] != "wav":
         filename = filename.strip() + ".wav"
     samplerate = sounddevice.query_devices()[device_id]["default_samplerate"]
-    threading.Thread(target=start_recording, args=(filename, device_id, int(samplerate))).start()
+    channels = sounddevice.query_devices()[device_id]["max_input_channels"]
+    threading.Thread(target=start_recording, args=(filename, device_id, int(samplerate), int(channels))).start()
 
 
 def stop_rec():
     global stop
     stop = True
-    root.destroy()
 
 
-"""
-get device list with: 'python3 -m sounddevice'
-then check for devices with at least one input channel
-adjust device_id accordingly
-"""
-if __name__ == "__main__":
-    stop = False
-    root = tkinter.Tk()
-    tkinter.Button(root, text="Start",
-                   command=lambda: prepare_rec(filename="Dateiname.wav", device_id=1)).pack()
-    tkinter.Button(root, text="Stop", command=stop_rec).pack()
-    root.mainloop()
---
+++ video_recording_stream.py
@@ -0,0 +1,32 @@
+import datetime
+import requests
+import threading
+
+global finished
+
+
+def record(url, filename):
+    file = open(filename, 'wb')
+    chunk_size = 1024
+
+    with requests.Session() as s:
+        response = s.get(url, stream=True)
+        for chunk in response.iter_content(chunk_size=chunk_size):
+            if chunk:
+                file.write(chunk)
+        file.close()
+    global finished
+    finished = True
+
+
+# example URL: "https://static-eus-rs.wondershare.com/Filmstock/file/s5/7a184ee5c5152b6f246640655570ec4e.mp4"
+def prepare_rec(url, filename=None):
+    s = requests.Session()
+    s.get(url, stream=True)
+    s.close()
+    if not filename:
+        filename = "str_" + datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S') + ".mp4"
+    if filename.strip()[-3:] != "mp4":
+        filename = filename.strip() + ".mp4"
+    filename = "./media/" + filename
+    threading.Thread(target=record, args=(url, filename,)).start()
